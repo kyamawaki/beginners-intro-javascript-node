2eefcafd0affd5afce0406d29bbcb63d14dd0497
yamawaki kenichi <yamawaki.mob@gmail.com>
2021-12-16 09:26:52 +0900

    06-strings

---
+++ string_concatenation.js
@@ -0,0 +1,17 @@
+let str1 = "Hello";
+let str2 = "World";
+
+// use + operator
+console.log(str1 + str2);
+console.log(str1 + "Big" + str2);
+
+// adding spacing
+str1 = "Hello ";
+console.log(str1 + str2);
+console.log(str1 + "Big " + str2);
+
+// 数値の1が"1"に変換され"1"+"2"⇒"12"となる
+let num1 = 1;
+let num2 = "2";
+console.log(num1 + num2);
+console.log(num1 + 1);
