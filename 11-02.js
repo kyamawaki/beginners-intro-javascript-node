2c380ba231df5a6c03c39332dbf3326428ea58ec
yamawaki kenichi <yamawaki.mob@gmail.com>
2021-12-27 09:38:02 +0900

    commit case-sensitive

---
+++ 11-02.js
@@ -0,0 +1,9 @@
+const _status = "error";
+
+if (_status === "ERROR") {
+    console.log("Something went wrong! -1-");
+} else if (_status.toUpperCase() === "ERROR") {
+    console.log("Something went wrong! -2-");
+} else {
+    console.log("Looks great!");
+}
