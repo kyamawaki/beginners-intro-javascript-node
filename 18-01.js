72f4367c239c7c36ae90ea222eb1134ede9f3116
yamawaki kenichi <yamawaki.mob@gmail.com>
2022-02-07 09:57:20 +0900

    コメント追加

--- 18-01.js
+++ 18-01.js
@@ -1,14 +1,34 @@
+// requireはモジュール化されたJavaScriptファイルを読み込む
+// モジュールとは機能ごとに分割したjsファイル
+// importもモジュールを読み込む方法だが、こちらはES6の仕様でnodeはrequireを使う
+
+// 次の行はdotenvモジュールを呼び出し、configメソッドを使用している
 require('dotenv').config();
 //console.log(process.env);
 
+// expressモジュールを読み込んで使えるようにする
+// ちなみにexpressはNode JS用のアプリケーションフレームワーク
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
+// expressアプリケーションを作成
+const app = express();
+//console.log(app);
+
+// 条件分岐(C#の3項演算子に近い。
+// process.env.PORGがnull/emptyでなければPORTを返し、そうでなけれな3000を返す)
+const port = process.env.PORT || 3000;
+console.log(port);
+
+// http://localhost:portで'Hello World!'を返す
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+// http://localhost:port/testで'Hello World! test'を返す
+app.get('/test', (req, res) => {
+    res.send('Hello World! test');
+});
 
+// HTTPリクエストをリッスンする
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:3000');
 })
